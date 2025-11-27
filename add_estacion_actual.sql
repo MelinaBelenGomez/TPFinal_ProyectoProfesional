355573d035a1637628d09e84687faf695d3c703d
Federico Zapata <federico11zapata@gmail.com>
2025-11-27 04:10:53 -0300

    deleted:    add_estacion_actual.sql 	deleted:    config_produccion_table.sql 	deleted:    database_script.sql

--- add_estacion_actual.sql
+++
@@ -1,2 +0,0 @@
--- Agregar columna estacion_actual a lote_proceso
-ALTER TABLE lote_proceso ADD COLUMN estacion_actual VARCHAR(50);
--- database_script.sql
+++
@@ -1,9 +0,0 @@
--- Tabla para resumen de desperdicios por orden de producción
--- IMPORTANTE: Ejecutar antes: ALTER TABLE resumen_desperdicio DROP CONSTRAINT IF EXISTS resumen_desperdicio_id_op_key;
-CREATE TABLE IF NOT EXISTS resumen_desperdicio (
-    id SERIAL PRIMARY KEY,
-    id_op INT NOT NULL REFERENCES orden_produccion(id_op),
-    motivo_principal VARCHAR(50) NOT NULL,
-    sku_mayor_desperdicio VARCHAR(50) NOT NULL REFERENCES producto(sku),
-    gramos_desperdiciados INT NOT NULL DEFAULT 0
-);
